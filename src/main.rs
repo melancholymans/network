ac677edcfb139bd68bc3013664e5380bee5f0b2e
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-24 18:50:50 +0900

    list8-2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use reqwest;
+
+fn main()->Result<(),Box<dyn Error>>{
+    let url = "http://www.rustinaction.com";
+    let mut response = reqwest::get(url)?;
+    let content = response.text()?;
+    print!("{}",content);
+    Ok(())
 }
